657246b512450957547447c7b2551a2cb8b9571d
yogeshsadgir0005 <yogeshsadgir05@gmail.com>
2025-08-24 23:44:53 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174'],
+  origin: ['http://localhost:5173', 'http://localhost:5174','https://innovative-staffing.vercel.app/'],
   credentials: true,
 }));
 app.use(express.json());
