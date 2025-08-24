f35cffa738a873c53020cfc66ec3e21f1f1ecb62
yogeshsadgir0005 <yogeshsadgir05@gmail.com>
2025-08-25 00:17:40 +0530

    Update api.js

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://innovative-backend.onrender.com/api', // adapt as needed
+  baseURL: 'https://innovative-staffing-1.onrender.com', // adapt as needed
 });
 
 export default api;
